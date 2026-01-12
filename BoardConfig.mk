a82c908e5b6da680a8f858136d36199a06ef215f
Moni <2793155371@qq.com>
2026-01-12 12:51:12 +0800

    Add the required drivers for TB520FU

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -181,7 +181,7 @@ TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 TW_INCLUDE_LPDUMP := true
 TW_INCLUDE_LPTOOLS := true
-TW_LOAD_VENDOR_MODULES := "qcom_pil_info.ko qmi_helpers.ko qcom_smd.ko qcom_glink.ko qcom_glink_smem.ko rproc_qcom_common.ko qcom_sysmon.ko qcom_q6v5.ko qcom_ramdump.ko qcom_q6v5_pas.ko snd_event_dlkm.ko pdr_interface.ko q6_pdr_dlkm.ko q6_notifier_dlkm.ko gpr_dlkm.ko spf_core_dlkm.ko adsp_loader_dlkm.ko"
+TW_LOAD_VENDOR_MODULES := "qcom_pil_info.ko qmi_helpers.ko qcom_smd.ko qcom_glink.ko qcom_glink_smem.ko rproc_qcom_common.ko qcom_sysmon.ko qcom_q6v5.ko qcom_ramdump.ko qcom_q6v5_pas.ko snd_event_dlkm.ko pdr_interface.ko q6_pdr_dlkm.ko q6_notifier_dlkm.ko gpr_dlkm.ko spf_core_dlkm.ko adsp_loader_dlkm.ko i2c-msm-geni.ko spi-msm-geni.ko panel_event_notifier.ko nvt_36xxx.ko"
 TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI := true
 TW_HAS_EDL_MODE := true
 
